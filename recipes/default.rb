caa1db70faf2d9d6433d3ccaa0e35d0d6a7b75c5
jcarapet <jeffcarapetyan@gmail.com>
2014-02-21 09:53:48 -0600

    added a space

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 Chef::Log.info("this is a test of whether jenkins can work.")
+
 case node['platform_family']
 when "debian"
 	include_recipe "df_java::oracle_ubuntu"
