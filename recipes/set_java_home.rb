9b474cb8a1ad436f0d49e435dc1f95ea2230ebcb
jcarapet <jeffcarapetyan@gmail.com>
2014-03-10 15:27:16 -0500

    adjusted path addition to prevent overwrite on /etc/profile

--- recipes/set_java_home.rb
+++ recipes/set_java_home.rb
@@ -7,42 +7,28 @@ directory "/etc/profile.d" do
 end
 case node['platform_family']
 when "debian" 
-file "/etc/profile" do 
-	new_lines = "JAVA_HOME=/usr/local/java/jdk1.7.0_51\n
+
+file "/etc/profile.d/java.sh" do 
+user "root" 
+group "root"
+mode "0755"
+content "JAVA_HOME=/usr/local/java/jdk1.7.0_51\n
 PATH=$PATH:$HOME/bin:$JAVA_HOME/bin\n
 export JAVA_HOME\n
 export PATH"
-
-only_if do 
-	current_content = File.read("/etc/profile")
-	current_content.index("JAVA_HOME").nil?
+action :create 
 end
 
-  mode 00755
-  user "root"
-  group "root"
-  current_content = File.read("/etc/profile")
-  new_content = current_content + new_lines
-  content "#{new_content}"
-end
 when "rhel"
-  file "/etc/profile" do 
-  new_lines = "JAVA_HOME=/usr/java/jdk1.7.0_51\n
+  file "/etc/profile/java.sh" do 
+user "root" 
+group "root"
+mode "0755"
+content "JAVA_HOME=/usr/java/jdk1.7.0_51\n
 PATH=$PATH:$HOME/bin:$JAVA_HOME/bin\n
 export JAVA_HOME\n
 export PATH"
-
-only_if do 
-  current_content = File.read("/etc/profile")
-  current_content.index("JAVA_HOME").nil?
-end
-
-  mode 00755
-  user "root"
-  group "root"
-  current_content = File.read("/etc/profile")
-  new_content = current_content + new_lines
-  content "#{new_content}"
+action :create
 end
 end
 
